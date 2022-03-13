e1d5bf1bb3264fbc175308211aec999dc6bbf920
maks2601 <47176065+maks2601@users.noreply.github.com>
2022-03-13 21:17:02 +0200

    Genesis setup

---
+++ Assets/DimensionalDoors/Sources/Components/PositionComponent.cs
@@ -0,0 +1,10 @@
+﻿using JCMG.EntitasRedux;
+
+namespace DimensionalDoors.Sources.Components
+{
+    public class PositionComponent : IComponent
+    {
+        public int x;
+        public int y;
+    }
+}
